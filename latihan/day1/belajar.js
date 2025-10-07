b7c6e0fa64f467feadd1d1c922ec24774698211f
RafidanAnwar <rafidananwar24@gmail.com>
2025-10-07 15:50:04 +0800

    upload tugas javascript

---
+++ latihan/day1/belajar.js
@@ -0,0 +1,21 @@
+// let nama = "Budi";
+// let umur = 25;
+
+// const tempatLahir = "Jakarta";
+
+// umur = 26;
+
+// console.log("Nama:", nama);
+// console.log("Umur:", umur);
+// console.log("Tempat Lahir:", tempatLahir);
+
+let isMahasiswa = true;
+let hobi = ["membaca", "bermain game", "olahraga"];
+let dataPribadi = {
+    pekerjaan: "Developer",
+    status: "lajang"
+};
+
+console.log("Apakah dia mahasiswa?", isMahasiswa);
+console.log("Hobi petamanya adalah:", hobi[0]);
+console.log("Status pernikahan:", dataPribadi.status);
